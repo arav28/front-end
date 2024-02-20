460a5ffbcb4193f26a40327c46c4a253cc05df13
aruve <aruve28@gmail.com>
2024-02-20 15:30:55 -0500

    fixed search button and search

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styles from "./Navbar.module.css";
 import { getImageUrl } from "../utils";
-
+import { FaSearch } from "react-icons/fa";
 
 export const Navbar = () => {
   const [menuDraw, setMenuDraw] = useState(false);
@@ -16,15 +16,25 @@ export const Navbar = () => {
           placeholder="Search items..."
           className={styles.inputbar}
         />
+        <button className="text-white text-lg bg-transparent">
+          <FaSearch/>
+        </button>
       </form>
       <div className={styles.menu}>
         <img
           className={styles.menuBtn}
-          src={menuDraw ? getImageUrl("nav/closeIcon.png") : getImageUrl("nav/menuIcon.png")}
+          src={
+            menuDraw
+              ? getImageUrl("nav/closeIcon.png")
+              : getImageUrl("nav/menuIcon.png")
+          }
           alt="Menu Icon"
           onClick={() => setMenuDraw(!menuDraw)}
         />
-        <ul className={`${styles.menuItems} ${menuDraw && styles.menuOpen}`} onClick={() => setMenuDraw(false)}>
+        <ul
+          className={`${styles.menuItems} ${menuDraw && styles.menuOpen}`}
+          onClick={() => setMenuDraw(false)}
+        >
           <li>
             <a href="#Home"> Home </a>
           </li>
