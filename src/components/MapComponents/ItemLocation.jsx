95c1d76b628ddf69df7af3deedcb4898026ee022
aruve <aruve28@gmail.com>
2024-03-14 17:15:49 -0400

    MapStyling css

--- src/components/MapComponents/ItemLocation.jsx
+++ src/components/MapComponents/ItemLocation.jsx
@@ -1,3 +1,5 @@
+// ItemLocation.jsx
+
 import React from "react";
 import MapComponent from "./Maps";
 import { useDispatch, useSelector } from "react-redux";
@@ -6,6 +8,8 @@ export default function ItemLocation() {
   const { currItemLocation } = useSelector((state) => state.item_mod);
   return (
     <div>
+      <h1>Item Location</h1>
+      <p>Information about the item or location goes here.</p>
       <MapComponent selectedLocation={currItemLocation} />
     </div>
   );
--- src/components/MapComponents/Maps.jsx
+++ src/components/MapComponents/Maps.jsx
@@ -1,6 +1,8 @@
+// MapComponent.jsx
+
 import React from "react";
-import { GoogleMap, useLoadScript, MarkerF } from "@react-google-maps/api";
-import { REACT_APP_GOOGLE_MAPS_KEY } from "../../constants/constants"
+import { GoogleMap, useLoadScript, Marker } from "@react-google-maps/api";
+import { REACT_APP_GOOGLE_MAPS_KEY } from "../../constants/constants";
 
 const MapComponent = ({ selectedLocation }) => {
   const { isLoaded, loadError } = useLoadScript({
@@ -11,21 +13,25 @@ const MapComponent = ({ selectedLocation }) => {
     mapRef.current = map;
   }, []);
   if (loadError) return "Error";
-  if (!isLoaded) return "Maps";
+  if (!isLoaded) return "Loading Maps...";
 
   return (
-    <div style={{ marginTop: "50px" }}>
+    <div style={{ marginTop: "20px" }}>
       <GoogleMap
         mapContainerStyle={{
-          height: "800px",
+          width: "100%",
+          height: "400px",
         }}
         center={selectedLocation}
         zoom={13}
         onLoad={onMapLoad}
       >
-        <MarkerF
+        <Marker
           position={selectedLocation}
-          icon={"http://maps.google.com/mapfiles/ms/icons/green-dot.png"}
+          icon={{
+            url: "http://maps.google.com/mapfiles/ms/icons/green-dot.png",
+            scaledSize: new window.google.maps.Size(30, 30),
+          }}
         />
       </GoogleMap>
     </div>
