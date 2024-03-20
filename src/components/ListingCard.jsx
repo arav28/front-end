b11163a6d57b9d0b82a465c14bce30852cc2d83f
SnehaManohar23 <snemanoh@iu.edu>
2024-03-20 18:04:28 -0400

    updated owner listings

--- src/components/ListingCard.jsx
+++ src/components/ListingCard.jsx
@@ -1,17 +1,29 @@
 import React from 'react';
+const formatDate = (dateString) => {
+  const options = { year: 'numeric', month: 'long', day: 'numeric' }; // Choose options that suit your needs
+  return new Date(dateString).toLocaleDateString(undefined, options); // The 'undefined' argument uses the browser's default locale
+};
+const ListingCard = ({ car, onDelete, onUpdate }) => {
+  const availableFromDate = formatDate(car.availableFrom);
+  const availableToDate = formatDate(car.availableTo);
 
-const ListingCard = ({ car, onDelete, onEdit }) => {
   return (
-    <div className="flex justify-between items-center bg-white p-4 rounded-lg shadow-md">
-      <div className="flex flex-col">
-        <div className="p-1">
-          <div className="uppercase tracking-wide text-sm text-black font-bold">{car.carMake}</div>
-          <a href="#" className="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{car.carModel}</a>
-          <div className="uppercase tracking-wide text-sm text-black font-bold">{car.year}</div>
-        </div>
+    <div className="flex flex-col bg-white p-4 rounded-lg shadow-md mb-4">
+      <div className="p-1 mb-4">
+        <div className="uppercase tracking-wide text-sm text-black font-bold">{car.carMake} {car.carModel}</div>
+        <p className="text-gray-700 text-base">Year: {car.year}</p>
+
+        <p className="text-gray-700 text-base">Price Per Day: ${car.pricePerDay}</p>
+        <p className="text-gray-700 text-base">Available From: {availableFromDate}</p>
+        <p className="text-gray-700 text-base">Available To: {availableToDate}</p>
+
+
       </div>
-      <div className="flex items-center">
-        <button onClick={() => onDelete(car.id)} className="bg-red-500 text-white px-2 py-1 rounded hover:bg-red-400 transition duration-300">
+      <div className="flex justify-end space-x-3">
+        <button onClick={() => onUpdate(car)} className="bg-blue-500 text-white px-3 py-2 rounded hover:bg-blue-400 transition duration-300">
+          Edit
+        </button>
+        <button onClick={() => onDelete(car._id)} className="bg-red-500 text-white px-3 py-2 rounded hover:bg-red-400 transition duration-300">
           Delete
         </button>
       </div>
